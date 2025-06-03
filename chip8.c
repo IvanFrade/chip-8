69227f5569c31fa86e48692ae55c769a47e29138
IvanFrade <fradellin.ivan@gmail.com>
2025-06-03 19:03:12 +0200

    Add SDL init boilerplate

--- chip8.c
+++ chip8.c
@@ -1,9 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
 #include <SDL.h>
+
+bool sdlInit() {
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER) != 0) {
+        SDL_Log("Unable to initialize SDL: %s\n", SDL_GetError());
+        return false;
+    }
+
+    return true; // SDL init successful
+}
+
+void cleanup() {
+    SDL_Quit();
+}
 
 int main(int argc, char **argv) {
     (void)argc;
     (void)argv;
 
+    // SDL initialization
+    if (!sdlInit()) exit(EXIT_FAILURE);
+
+    // SDL cleanup
+
     puts("Hello world!");
+
+    exit(EXIT_SUCCESS);
 }
