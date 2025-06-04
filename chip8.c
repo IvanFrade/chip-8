fd7ed5d48929e01c907679bab2c01f4c943535bb
IvanFrade <fradellin.ivan@gmail.com>
2025-06-04 22:34:13 +0200

    Add state enum and chip8 strut

--- chip8.c
+++ chip8.c
@@ -7,11 +7,18 @@
 
 // Emulator config
 typedef struct {
-    uint32_t width;     // Window width
-    uint32_t height;    // Window height
-    uint32_t bg_color;  // Background Color, 
-    uint32_t fg_color;  // Foreground color, 
+    uint32_t width;         // Window width
+    uint32_t height;        // Window height
+    uint32_t scale_factor;  // Scale the original 64x32 pixel screen
+    uint32_t bg_color;      // Background Color, 
+    uint32_t fg_color;      // Foreground color, 
 } emulator_t;           
+
+typedef enum {
+    QUIT, 
+    RUNNING, 
+    PAUSED,
+} emulator_state_t;
 
 // SDL window
 typedef struct { 
@@ -19,11 +26,16 @@ typedef struct {
     SDL_Renderer *renderer;
 } sdl_t;
 
+typedef struct {
+    emulator_state_t state;
+} chip8_t;
+
 // Setup emulator config by passed in arguments
 bool emuConfigInit(emulator_t *emulator, const int argc, char **argv) {
     // Default config
-    emulator->width = 640;
-    emulator->height = 320;
+    emulator->width = 64;
+    emulator->height = 32;
+    emulator->scale_factor = 20;
     (void)argc;
     (void)argv;
 
@@ -43,12 +55,12 @@ bool sdlInit(const emulator_t emulator, sdl_t *sdl) {
         return false;
     }
 
-    sdl->window = SDL_CreateWindow("Chip 8",                   // Title
-                                    SDL_WINDOWPOS_CENTERED,     // X pos
-                                    SDL_WINDOWPOS_CENTERED,     // Y pos
-                                    emulator.width,            // Width
-                                    emulator.height,           // Height
-                                    0);                         // Flags
+    sdl->window = SDL_CreateWindow("Chip 8",                                // Title
+                                    SDL_WINDOWPOS_CENTERED,                 // X pos
+                                    SDL_WINDOWPOS_CENTERED,                 // Y pos
+                                    emulator.width * emulator.scale_factor, // Width
+                                    emulator.height * emulator.scale_factor,// Height
+                                    0);                                     // Flags
     if (sdl->window == NULL) {
         SDL_Log("Could not create a window: %s\n", SDL_GetError());
         return false;
@@ -63,6 +75,11 @@ bool sdlInit(const emulator_t emulator, sdl_t *sdl) {
     }
 
     return true; // SDL init successful
+}
+
+bool chip8Init(chip8_t *chip8) {
+    chip8 = chip8;
+    return true;
 }
 
 // Screen clear to bg color
@@ -80,6 +97,21 @@ void updateScreen(const sdl_t sdl) {
     SDL_RenderPresent(sdl.renderer);
 }
 
+void handleInput() {
+    SDL_Event event;
+
+    while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+        case SDL_QUIT:
+            // close window
+            break;
+        
+        default:
+            break;
+        }
+    }
+}
+
 void cleanup(sdl_t *sdl) {
     SDL_DestroyRenderer(sdl->renderer);
     SDL_DestroyWindow(sdl->window);
@@ -90,10 +122,12 @@ int main(int argc, char **argv) {
     // Init structs to allocate memory
     sdl_t sdl = {0};
     emulator_t emulator = {0};
+    chip8_t chip8 = {0};
 
     // Init emu configuration
     if (!emuConfigInit(&emulator, argc, argv)) exit(EXIT_FAILURE);    
     if (!sdlInit(emulator, &sdl)) exit(EXIT_FAILURE);
+    if (!chip8Init(&chip8)) exit(EXIT_FAILURE);
 
     // Initial renderer clear
     clearScreen(emulator, sdl);
