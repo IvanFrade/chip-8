17950b55958ccd6ef95ed5dcc96fe7b0c789cb2b
IvanFrade <fradellin.ivan@gmail.com>
2025-06-04 21:13:52 +0200

    Add main loop, clear and update functions
    Added main execution loop (for now, just an
    update screen and a random delay). Also moved
    clear and update screen into their own functions.
    Added background and foreground color to
    emulator config.

--- chip8.c
+++ chip8.c
@@ -7,9 +7,11 @@
 
 // Emulator config
 typedef struct {
-    uint32_t width;  // Window width
-    uint32_t height; // Window height
-} emulator_t;
+    uint32_t width;     // Window width
+    uint32_t height;    // Window height
+    uint32_t bg_color;  // Background Color, 
+    uint32_t fg_color;  // Foreground color, 
+} emulator_t;           
 
 // SDL window
 typedef struct { 
@@ -24,6 +26,10 @@ bool emuConfigInit(emulator_t *emulator, const int argc, char **argv) {
     emulator->height = 320;
     (void)argc;
     (void)argv;
+
+    // Render color 
+    emulator->fg_color = 0xFFFFFFFF; // White
+    emulator->bg_color = 0x000000FF; // Black
 
     // TODO: set config according to args
 
@@ -56,13 +62,22 @@ bool sdlInit(const emulator_t emulator, sdl_t *sdl) {
         return false;
     }
 
-    SDL_SetRenderDrawColor(sdl->renderer, 255, 255, 255, 255);
-    SDL_RenderClear(sdl->renderer);
-    SDL_RenderPresent(sdl->renderer);
-
-    SDL_Delay(3000); //test
-
     return true; // SDL init successful
+}
+
+// Screen clear to bg color
+void clearScreen(const emulator_t emulator, sdl_t sdl) {
+    const uint32_t r = (emulator.bg_color >> 24) & 0x000000FF;
+    const uint32_t g = (emulator.bg_color >> 16) & 0x000000FF;
+    const uint32_t b = (emulator.bg_color >> 8) & 0x000000FF;
+    const uint32_t a = emulator.bg_color & 0x000000FF;
+    
+    SDL_SetRenderDrawColor(sdl.renderer, r, g, b, a);
+    SDL_RenderClear(sdl.renderer);
+}
+
+void updateScreen(sdl_t sdl) {
+    SDL_RenderPresent(sdl.renderer);
 }
 
 void cleanup(sdl_t *sdl) {
@@ -72,13 +87,22 @@ void cleanup(sdl_t *sdl) {
 }
 
 int main(int argc, char **argv) {
+    // Init structs to allocate memory
     sdl_t sdl = {0};
     emulator_t emulator = {0};
 
     // Init emu configuration
     if (!emuConfigInit(&emulator, argc, argv)) exit(EXIT_FAILURE);    
-
     if (!sdlInit(emulator, &sdl)) exit(EXIT_FAILURE);
+
+    // Initial renderer clear
+    clearScreen(emulator, sdl);
+
+    // Main loop
+    while (true) {
+        updateScreen(sdl);
+        SDL_Delay(3000); //test
+    }
 
     // SDL cleanup
     cleanup(&sdl);
